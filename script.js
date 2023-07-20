8ac04ebef627a2babd0020d2c1cdb09d39c4f11f
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-20 09:45:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const newElement = document.createElement("div");
+
+newElement.id = "browser-info";
+
+newElement.innerHTML = "You are using " + navigator.appName + " version " + navigator.appVersion
+const body = document.getElementsByTagName("body")[0];
+
+body.appendChild(newElement);
